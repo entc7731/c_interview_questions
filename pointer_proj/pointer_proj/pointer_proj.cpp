227ee0166e7063affc5d5a11ff30b65dcbecb845
suhelmulla <suhel19690@gmail.com>
2022-09-18 17:31:48 +0530

    Is an array really sorted, sorted or not sorted
    Is an array really sorted, sorted or not sorted

--- pointer_proj/pointer_proj/pointer_proj.cpp
+++ pointer_proj/pointer_proj/pointer_proj.cpp
@@ -1,40 +1,51 @@
-// Online C compiler to run C program online
+// Is an array really sorted, sorted or not sorted
+
 #include <stdio.h>
 
+int array_type(int *arr, int size, int *sorting_type_loc);
 int main() {
-    // Pointer Arithmetic
-    int num = 30;
-    int arr[3] = { 0,5,6 };
-    int* p;
-    double* q;
 
-    printf("\nnum (in 'd' format specifier) = %d", num);
-    printf("\nnum (in 'p' format specifier) = %p", num);
-    printf("\n");
+    int arr1[] = { 1,2,3,4,5,6,7,8 };
+    int arr2[] = { 1,2,3,4,4,5,6,7 };
+    int arr3[] = { 4,6,7,4,2,7,8,9 };
+    int sorting_type = 0;
+    int result = 0;
+    
+    result = array_type(arr1, (sizeof(arr1) / sizeof(int)), &sorting_type);
 
-    p = &num;
-
-    printf("\nnum addr (in 'd' format specifier) = %d", p);
-    printf("\nnum addr (in 'p' format specifier) = %p", p);
-    printf("\n");
-
-    printf("\nsize of addition of 2 integers = %d", sizeof(num + num));
-    printf("\nsize of addition of 2 sizeof = %d", (sizeof(num) + sizeof(num)));
-    printf("\n");
-
-    // Here it is treated as integer, thus 4.
-    printf("\nsize of character = %d", sizeof('a'));
-    printf("\nsize of string= %d", sizeof("a"));
-    printf("\n");
-
-    printf("\nsize of array= %d", sizeof(arr));
-    printf("\n");
-
-    p = arr;
-    printf("\nsize of int pointer = %d", sizeof(p)); // 4 in 32 bit environment, 8 in 64 bit env.
-    printf("\nsize of double pointer = %d", sizeof(q));
-    printf("\n");
-
+    if ((result == 1) && (sorting_type == 1))
+        printf("\n The array is REALLY sorted.");
+    else if((result == 1) && (sorting_type == 0))
+        printf("\n The array is sorted.");
+    else if ((result == 0) && (sorting_type == 0))
+        printf("\n The array is not sorted.");
+    else
+    {
+        // intentionally kept blank
+    }
     return 0;
 
+}
+
+int array_type(int* arr, int size, int* sorting_type_loc)
+{
+    *sorting_type_loc = 1;
+
+    for (int count = 1; count < (size - 1); count++)
+    {
+        if (arr[count] < arr[count - 1])
+        {
+            *sorting_type_loc = 0;
+            return 0;
+        }
+        else if (arr[count] == arr[count - 1])
+        {
+            *sorting_type_loc = 0;
+        }
+        else
+        {
+            // intentionally kept blank
+        }
+    }
+    return 1;
 }
